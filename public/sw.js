36b8306fcdbad74060974d3f1856bde3c92617f6
otavioag <otavio.luis.aguiar@gmail.com>
2019-09-03 15:06:18 -0300

    feat: PWA

---
+++ public/sw.js
@@ -0,0 +1,43 @@
+var CACHE_NAME = 'myreads-app';
+
+var urlsToCache = [
+  '/',
+  '/favicon.ico',
+  '/css?family=Roboto:400,700'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+        .then(function (cache) {
+          return cache.addAll(urlsToCache);
+        })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+        .then(function (response) {
+            if (response) {
+              return response;
+            }
+            return fetch(event.request).then(
+              function(response) {
+                if(!response || response.status !== 200 || response.type !== 'basic') {
+                  return response;
+                }
+                
+                var responseToCache = response.clone();
+    
+                caches.open(CACHE_NAME)
+                  .then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                  });
+    
+                return response;
+              }
+            );
+        })
+  );
+});
