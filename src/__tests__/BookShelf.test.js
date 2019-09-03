30541929ae49358c7698dee66d60e276e06eed24
otavioag <otavio.luis.aguiar@gmail.com>
2019-09-03 01:38:44 -0300

    fix: Changes for tests to work after updating dependencies

--- src/__tests__/BookShelf.test.js
+++ src/__tests__/BookShelf.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import BookShelf from '../components/book/BookShelf';
+import Book from '../components/book/Book';
 
 describe('BookShelf', () => {
   it('renders without crashing - with book data', () => {
     const wrapper = shallow(<BookShelf
       title=''
-      shelf='read'
-      books={[{id: 'id', shelf: 'read'}]}
-      handleShelfChange={jest.fn()}/>
+      books={[<Book
+        key='key'
+        book={{title: "React", authors: ["Nils Hartmann", "Oliver Zeigermann"], imageLinks: {thumbnail: "http://books.google.com/books/content?id=IOejDAAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"}, shelf: 'read'}}
+        handleShelfChange={jest.fn()} />]}/>
     );
     expect(wrapper).toHaveLength(1);
     expect(wrapper).toMatchSnapshot();
